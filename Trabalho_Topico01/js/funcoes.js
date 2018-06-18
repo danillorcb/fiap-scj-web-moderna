b6f86bddd69af8ae826e870039689f1b43e13ea1
Danillo Ramos <danilloramosbezerra@gmail.com>
2018-06-17 21:53:22 -0300

    add folder Trabalho_Topico01

---
+++ Trabalho_Topico01/js/funcoes.js
@@ -0,0 +1,32 @@
+var assuntos = [
+    "Desenvolvimento de sistemas",
+    "Oportunidade de emprego",
+    "Música, Rock e instrumentos musicais",
+    "Quadrinhos, séries ou cinema"
+];
+
+var lista = document.getElementById("listaAssuntos");
+
+function limparLista() {
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+}
+
+function exibirAssuntos() {
+    limparLista();
+
+    //Acrescentando item default
+    var option = document.createElement("option");
+    $(option).html("Motivo do contato/Assunto");
+    lista.appendChild(option);
+
+    $.each(assuntos, function(i, assunto){
+        var option = document.createElement("option");
+        $(option).html(assunto);
+        $(option).attr("value", i);
+        lista.appendChild(option);
+    });
+}
+
+exibirAssuntos();
